3123dbc0c899c0a5b3f826d70faca1276f959623
slayerstokely <stokely@slayers.ru>
2023-11-29 06:38:08 +0200

    week2 day2 xp ninja

--- Week2/Day2/ExercisesXP/ExerciseXPNinja.py
+++ Week2/Day2/ExercisesXP/ExerciseXPNinja.py
@@ -8,26 +8,26 @@
 
 import math
 
-# C = 50
-# H = 30
+C = 50
+H = 30
 
-# user_numbers = input('Give me a comma-separated list of numbers: ')
-# numbers_list = [int(i) for i in user_numbers.split(',')]
-# results_list = []
+user_numbers = input('Give me a comma-separated list of numbers: ')
+numbers_list = [int(i) for i in user_numbers.split(',')]
+results_list = []
 
-# for D in numbers_list:
-#     results_list.append(str(math.floor(math.sqrt((2 * C * D)/H))))
+for D in numbers_list:
+    results_list.append(str(math.floor(math.sqrt((2 * C * D)/H))))
 
-# results = ','.join(results_list)
-# print(results)
+results = ','.join(results_list)
+print(results)
 
 # Exercise 2 : List of integers
 # Given a list of 10 integers to analyze
 # Store the list of numbers in a variable.
 # Print the following information:
-# a. The list of numbers printed in a single line
-# b. The list of numbers sorted in descending order (largest to smallest)
-# c. The sum of all the numbers
+# The list of numbers printed in a single line
+# The list of numbers sorted in descending order (largest to smallest)
+# The sum of all the numbers
 # A list containing the first and the last numbers.
 # A list of all the numbers greater than 50.
 # A list of all the numbers smaller than 10.
@@ -35,21 +35,68 @@ import math
 # The numbers without any duplicates also print how many numbers are in the new list.
 # The average of all the numbers.
 # The largest number.
-# 10.The smallest number.
-# 11.Bonus: Find the sum, average, largest and smallest number without using built in functions.
-# 12.Bonus: Instead of using pre-defined lists of numbers, ask the user for 10 numbers between -100 and 100. Ask the user for an integer between -100 and 100 – repeat this question 10 times. Each number should be added into a variable that you created earlier.
-# 13.Bonus: Instead of asking the user for 10 integers, generate 10 random integers yourself. Make sure that these random integers are between -100 and 100.
-# 14.Bonus: Instead of always generating 10 integers, let the amount of integers also be random! Generate a random positive integer no smaller than 50.
-# 15.Bonus: Will the code work when the number of random numbers is not equal to 10?
+# The smallest number.
+# Bonus: Find the sum, average, largest and smallest number without using built in functions.
+# Bonus: Instead of using pre-defined lists of numbers, ask the user for 10 numbers between -100 and 100. Ask the user for an integer between -100 and 100 – repeat this question 10 times. Each number should be added into a variable that you created earlier.
+# Bonus: Instead of asking the user for 10 integers, generate 10 random integers yourself. Make sure that these random integers are between -100 and 100.
+# Bonus: Instead of always generating 10 integers, let the amount of integers also be random! Generate a random positive integer no smaller than 50.
+# Bonus: Will the code work when the number of random numbers is not equal to 10?
 
 numbers = [3, 47, 99, -80, 22, 97, 54, -23, 3, 3, 5, 7, 99]
 
 print(numbers)
-print(sorted(numbers, reverse=True))
-print(sum(numbers))
-print([numbers[0], numbers[-1]])
-print([num for num in numbers if num > 50])
-print([num for num in numbers if num < 50])
-print([num**2 for num in numbers if num >= 0])
+print(f'In descending order: {sorted(numbers, reverse=True)}')
+print(f'Sum: {sum(numbers)}')
+print(f'First: {numbers[0]}, last: {numbers[-1]}')
+print(f'Greater than 50: {[num for num in numbers if num > 50]}')
+print(f'Less than 10: {[num for num in numbers if num < 10]}')
+print(f'Squared: {[num**2 for num in numbers if num >= 0]}')
 
-list_dedup = []
+dedup = []
+[dedup.append(num) for num in numbers if num not in dedup]
+print(f'Without duplicates: {dedup}')
+print(f'Length without duplicates: {len(dedup)}')
+
+print(f'Average: {sum(numbers) // len(numbers)}')
+
+print(f'Min: {min(numbers)}')
+
+numbers_sum = 0
+numbers_len = 0
+numbers_max = numbers[0]
+numbers_min = numbers[0]
+
+for num in numbers:
+    numbers_sum += num
+    numbers_len += 1
+    if num > numbers_max:
+        numbers_max = num
+    if num < numbers_min:
+        numbers_min = num
+
+print(f'Sum: {numbers_sum}')
+print(f'Average: {numbers_sum // numbers_len}')
+print(f'Max: {numbers_max}')
+print(f'Min: {numbers_min}')
+
+# Exercise 3: Working on a paragraph
+# Find an interesting paragraph of text online. (Please keep it appropriate to the social context of our class.)
+# Paste it to your code, and store it in a variable.
+# Let’s analyze the paragraph. Print out a nicely formatted message saying:
+# How many characters it contains (this one is easy…).
+# How many sentences it contains.
+# How many words it contains.
+# How many unique words it contains.
+# Bonus: How many non-whitespace characters it contains.
+# Bonus: The average amount of words per sentence in the paragraph.
+# Bonus: the amount of non-unique words in the paragraph.
+
+story = """Did you ever hear the tragedy of Darth Plagueis The Wise? I thought not. It's not a story the Jedi would tell you. It's a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life… He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful… the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic. He could save others from death, but not himself."""
+
+print(f'The story of Darth Plagueis The Wise contains {len(story)} characters.')
+print(f'The story of Darth Plagueis The Wise contains {story.count(".") + story.count("?") + story.count("!") + story.count("…")} sentences.')
+print(f'The story of Darth Plagueis The Wise contains {len(story.split())} words.')
+print(f'The story of Darth Plagueis The Wise contains {len(set(story.split()))} unique words.')
+print(f'The story of Darth Plagueis The Wise contains {len(story) - story.count(" ")} non-whitespace characters.')
+print(f'The story of Darth Plagueis The Wise contains {(len(story.split())) // (story.count(".") + story.count("?") + story.count("!") + story.count("…"))} words per sentence on average.')
+print(f'The story of Darth Plagueis The Wise contains {len(story.split()) - len(set(story.split()))} non-unique words.')
---
+++ Week2/Day3/ExercisesXP/ExerciseXPNinja.py
@@ -0,0 +1,33 @@
+# Exercise 1 : Cars
+# Copy the following string into your code: "Volkswagen, Toyota, Ford Motor, Honda, Chevrolet".
+# Convert it into a list using Python (don’t do it by hand!).
+# Print out a message saying how many manufacturers/companies are in the list.
+# Print the list of manufacturers in reverse/descending order (Z-A).
+# Using loops or list comprehension:
+# Find out how many manufacturers’ names have the letter ‘o’ in them.
+# Find out how many manufacturers’ names do not have the letter ‘i’ in them.
+# Bonus: There are a few duplicates in this list:["Honda","Volkswagen", "Toyota", "Ford Motor", "Honda", "Chevrolet", "Toyota"]
+# Remove these programmatically. (Hint: you can use set to help you).
+# Print out the companies without duplicates, in a comma-separated string with no line-breaks (eg. “Acura, Alfa Romeo, Aston Martin, …”), also print out a message saying how many companies are now in the list.
+# Bonus: Print out the list of manufacturers in ascending order (A-Z), but reverse the letters of each manufacturer’s name.
+
+cars = "Volkswagen, Toyota, Ford Motor, Honda, Chevrolet"
+cars_list = cars.split(', ')
+
+print(f'There are {len(cars_list)} car manufacturers in the list.')
+print(sorted(cars_list, reverse=True))
+count_o = 0
+count_no_i = 0
+
+for name in cars_list:
+    if 'o' in name:
+        count_o += 1
+    if 'i' not in name:
+        count_no_i += 1
+
+print(f'There are {count_o} car manufacturers that have letter "o" in their name.')
+print(f'There are {count_no_i} car manufacturers that don\'t have letter "i" in their name.')
+
+cars_list_2 = ["Honda","Volkswagen", "Toyota", "Ford Motor", "Honda", "Chevrolet", "Toyota"]
+cars_list_2_dedup = set(cars_list_2)
+print(cars_list_2_dedup)
