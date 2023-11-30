2cc368de72b20c2f9173dc070a4129d6d736b933
arrenkae <stokely@slayers.ru>
2023-11-30 14:38:22 +0200

    finished daily challenge advanced

---
+++ Week2/Day5/DailyChallenge/DailyChallengeAdvanced.py
@@ -0,0 +1,19 @@
+# Advanced Algorithm
+# Here is a python code that generate a list of 20000 random numbers, called list_of_numbers.
+# Extend this code to guess how many couples of numbers in list_of_numbers sum to target_number.
+
+import random
+
+list_of_numbers = [random.randint(0, 10000) for i in range(20000)]
+target_number = 3728
+
+def target_sum_couples(numbers:list, target:int):
+    couples = []
+    for num1 in numbers:
+        for num2 in numbers:
+            if num1 + num2 == target:
+                couples.append((num1, num2))
+    count = len(set(couples))
+    return(count)
+
+print(target_sum_couples(list_of_numbers,target_number))
