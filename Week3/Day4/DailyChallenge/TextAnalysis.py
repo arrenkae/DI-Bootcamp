a04b7bd485245a659aef0f33fcc1309357cf4614
arrenkae <stokely@slayers.ru>
2023-12-06 16:10:09 +0200

    started daily challenge

---
+++ Week3/Day4/DailyChallenge/TextAnalysis.py
@@ -0,0 +1,75 @@
+# Daily challenge : Text Analysis
+
+# Part I
+
+# First, we will analyze a simple string, like “A good book would sometimes cost as much as a good house.”
+
+#     Create a class called Text that takes a string as an argument and store the text in a attribute.
+#     Hint: You need to manually copy-paste the text, straight into the code
+
+#     Implement the following methods:
+#         a method to return the frequency of a word in the text (assume words are separated by whitespace) return None or a meaningful message.
+#         a method that returns the most common word in the text.
+#         a method that returns a list of all the unique words in the text.
+
+
+# Part II
+
+# Then, we will analyze a text coming from an external text file. Download the_stranger.txt file.
+
+#     Implement a classmethod that returns a Text instance but with a text file:
+
+#         >>> Text.from_file('the_stranger.txt')
+
+#     Hint: You need to open and read the text from the text file.
+
+#     Now, use the provided the_stranger.txt file and try using the class you created above.
+
+
+# Bonus:
+
+#     Create a class called TextModification that inherits from Text.
+
+#     Implement the following methods:
+#         a method that returns the text without any punctuation.
+#         a method that returns the text without any english stop-words (check out what this is !!).
+#         a method that returns the text without any special characters.
+
+# Note: Feel free to implement/create any attribute, method or function needed to make this work, be creative :)
+
+from collections import Counter
+
+class Text:
+
+    def __init__(self, text):
+        self.text = text
+    
+    def wordcount(self):
+        STOP_WORDS = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 
+    'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't", 'would']
+        words = self.text.lower().split()
+        for word in words:
+            if word in STOP_WORDS:
+                words.remove(word)
+        return Counter(words)
+
+    def frequency(self, word):
+        if word in text:
+            return text.count(word)
+        else:
+            return None
+
+    def most_common(self):
+        min_count = 1
+        for word in text.split():
+            if text.count(word.lower()) > min_count and word.lower() not in STOP_WORDS:
+                min_count = text.count(text)
+                most_common = word.lower()
+        return(most_common)
+
+    # def unique_words(self):
+
+
+text1 = Text('A good book would sometimes cost as much as a good house sometimes.')
+
+print(text1.wordcount())
