ba40570de082ef8dd0edfa570fb9f8add022b676
slayerstokely <stokely@slayers.ru>
2023-12-06 20:25:46 +0200

    daily challenge

--- Week3/Day4/DailyChallenge/TextAnalysis.py
+++ Week3/Day4/DailyChallenge/TextAnalysis.py
@@ -37,39 +37,58 @@
 
 # Note: Feel free to implement/create any attribute, method or function needed to make this work, be creative :)
 
-from collections import Counter
-
 class Text:
 
     def __init__(self, text):
         self.text = text
+        self.wordcount = self.wordcount()
     
     def wordcount(self):
-        STOP_WORDS = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 
-    'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't", 'would']
-        words = self.text.lower().split()
+        STOP_WORDS = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her',
+        'hers', 'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is',
+        'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as',
+        'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up',
+        'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few',
+        'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now',
+        'ain', 'aren', 'couldn', 'didn', 'doesn', 'hadn', 'hasn', 'haven', 'isn', 'ma', 'mightn', 'mustn', 'needn', 'shan', 'shouldn', 'wasn', 'weren', 'won' 'wouldn', 'would']
+        remove = '''!()-—[]{};:'"\,<>./?@#$%^&*_~♦1234567890'''
+        words = []
+        for word in self.text.lower().split():
+            word_clean = word.lower().strip(remove).replace("'s",'').replace("'d",'').replace("'t",'').replace("'ll",'').replace("'re",'').replace("'ve",'')
+            if word_clean not in STOP_WORDS and len(word_clean) > 1:
+                words.append(word_clean)
+        wordcount = {}
         for word in words:
-            if word in STOP_WORDS:
-                words.remove(word)
-        return Counter(words)
+            wordcount.setdefault(word, 0)
+            wordcount[word] += 1
+        return wordcount
+
+    @classmethod
+    def from_file(cls, file):
+        with open(file,encoding = 'utf-8') as f:
+            text = f.read()
+        return cls(text)
 
     def frequency(self, word):
-        if word in text:
-            return text.count(word)
-        else:
-            return None
+        if word in self.wordcount:
+            return self.wordcount[word]
 
     def most_common(self):
-        min_count = 1
-        for word in text.split():
-            if text.count(word.lower()) > min_count and word.lower() not in STOP_WORDS:
-                min_count = text.count(text)
-                most_common = word.lower()
-        return(most_common)
+        max_count = max(self.wordcount.values())
+        return [word for word in self.wordcount if self.wordcount[word] == max_count]
 
-    # def unique_words(self):
+    def unique_words(self):
+        
+        return [word for word in self.wordcount if self.wordcount[word] == 1]
 
+text1 = Text('A good book would sometimes cost as much as a good house (sometimes).')
 
-text1 = Text('A good book would sometimes cost as much as a good house sometimes.')
+print(text1.frequency('good'))
+print(text1.most_common())
+print(text1.unique_words())
 
-print(text1.wordcount())
+stranger = Text.from_file('the_stranger.txt')
+
+print(stranger.unique_words())
+print(stranger.most_common())
+print(stranger.frequency('stranger'))
