898de82e95d1a508cf2aaae9460bdc1e2d4be214
slayerstokely <stokely@slayers.ru>
2023-12-07 06:21:54 +0200

    fixed variables

--- Week3/Day4/DailyChallenge/TextAnalysis.py
+++ Week3/Day4/DailyChallenge/TextAnalysis.py
@@ -39,12 +39,7 @@
 
 class Text:
 
-    def __init__(self, text):
-        self.text = text
-        self.wordcount = self.wordcount()
-    
-    def wordcount(self):
-        STOP_WORDS = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 
+    stop_words = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 
         'yourselves', 'he', "he'd", 'him', 'his', 'himself', 'she', "she'd", "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 
         'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 
         'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against',
@@ -53,11 +48,17 @@ class Text:
         'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 
         'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn',
         "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't", 'would']
-        remove = '''!()-—[]{};:'"\,<>./?@#$%^&*_~♦1234567890'''
+    special_chars = '''!()-—[]{};:'"\,<>./?@#$%^&*_~1234567890♦'''
+
+    def __init__(self, text):
+        self.text = text
+        self.wordcount = self.wordcount()
+    
+    def wordcount(self):
         words = []
         for word in self.text.lower().split():
-            word_clean = word.lower().strip(remove)
-            if word_clean not in STOP_WORDS and len(word_clean) > 1:
+            word_clean = word.lower().strip(Text.special_chars)
+            if word_clean not in Text.stop_words and len(word_clean) > 1:
                 words.append(word_clean)
         wordcount = {}
         for word in words:
