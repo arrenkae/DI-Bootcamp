81a2672d06e4f66d2e0225fe9f40b3bd78e85fd6
arrenkae <stokely@slayers.ru>
2023-12-10 13:31:22 +0200

    sql xp exercise

---
+++ Week4/Day1/ExerciseXP/ExerciseXP.sql
@@ -0,0 +1,35 @@
+CREATE TABLE items (
+	id serial PRIMARY KEY,
+	name varchar(255) NOT NULL,
+	price integer NOT NULL
+)
+
+CREATE TABLE customers (
+	id serial PRIMARY KEY,
+	firstname varchar(255) NOT NULL,
+	lastname varchar(255) NOT NULL
+)
+
+INSERT INTO items (name, price)
+VALUES ('Small Desk', 100),
+('Large Desk', 300),
+('Fan', 80)
+
+INSERT INTO customers (firstname, lastname)
+VALUES ('Greg', 'Jones'),
+('Sandra', 'Jones'),
+('Scott', 'Scott'),
+('Trevor', 'Green'),
+('Melanie', 'Johnson')
+
+SELECT * FROM items
+
+SELECT * FROM items WHERE price > 80
+
+SELECT * FROM items WHERE price <= 300
+
+SELECT * FROM customers WHERE lastname = 'Smith'
+
+SELECT * FROM customers WHERE lastname = 'Jones'
+
+SELECT * FROM customers WHERE lastname != 'Scott'
