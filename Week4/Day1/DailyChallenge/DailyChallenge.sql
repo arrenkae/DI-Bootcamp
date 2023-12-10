99cb8e3b68fd3f4c94f5da68422cb9ec39685b3a
arrenkae <stokely@slayers.ru>
2023-12-10 14:29:38 +0200

    daily challenge & xp exercise fixes

---
+++ Week4/Day1/DailyChallenge/DailyChallenge.sql
@@ -0,0 +1,34 @@
+CREATE TABLE actors(
+	actor_id SERIAL PRIMARY KEY,
+	first_name VARCHAR (50) NOT NULL,
+	last_name VARCHAR (100) NOT NULL,
+	age DATE NOT NULL,
+	number_oscars SMALLINT NOT NULL
+);
+
+INSERT INTO actors (first_name, last_name, age, number_oscars)
+VALUES('Matt','Damon','08/10/1970', 5);
+
+INSERT INTO actors (first_name, last_name, age, number_oscars)
+VALUES('George','Clooney','06/05/1961', 2);
+
+INSERT INTO actors (first_name, last_name, age, number_oscars)
+VALUES('Meryl','Streep','22/06/1949', 3);
+
+INSERT INTO actors (first_name, last_name, age, number_oscars)
+VALUES('Judi','Dench','09/12/1934', 1);
+
+INSERT INTO actors (first_name, last_name, age, number_oscars)
+VALUES ('Leonardo','DiCaprio','11/11/1974', 1),
+('Morgan','Freeman','01/06/1937', 1),
+('Dwayne','Johnson','02/05/1972', 0);
+
+SELECT * FROM actors;
+
+SELECT COUNT(actor_id)
+FROM actors;
+
+-- INSERT INTO actors (first_name)
+-- VALUES('Bob')
+-- returns an error because of not-null constraint
+
--- Week4/Day1/ExerciseXP/ExerciseXPGold.sql
+++ Week4/Day1/ExerciseXP/ExerciseXPGold.sql
@@ -1,6 +1,6 @@
-SELECT * FROM students WHERE id BETWEEN 1 AND 4 ORDER BY last_name
+SELECT * FROM students WHERE id BETWEEN 1 AND 4 ORDER BY last_name;
 
-SELECT * FROM students WHERE birth_date = (SELECT MIN(birth_date) FROM students)
+SELECT * FROM students WHERE birth_date = (SELECT MIN(birth_date) FROM students);
 
 SELECT * FROM students WHERE id > 2
 LIMIT 3
