a8227a560628014c1ebad15c16a32f5a316c2742
slayerstokely <stokely@slayers.ru>
2023-12-24 17:19:25 +0200

    daily challenge 1

---
+++ Week6/Day1/DailyChallenge/NotBad/notbad.js
@@ -0,0 +1,27 @@
+// 1. Create a variable called sentence. The value of the variable should be a string that contains the words “not” and “bad”.
+// For example, “The movie is not that bad, I like it”.
+
+// 2. Create a variable called wordNot where it’s value is the first appearance (ie. the position) of the substring “not” (from the sentence variable).
+
+// 3. Create a variable called wordBad where it’s value is the first appearance (ie. the position) of the substring “bad” (from the sentence variable).
+
+// 4. If the word “bad” comes after the word “not”, you should replace the whole “not…bad” substring with “good”, then console.log the result.
+// For example, the result here should be : “The movie is good, I like it”
+// If the word “bad” does not come after “not” or the words are not in the sentence, console.log the original sentence.
+
+let sentence = "The movie is not that bad, I like it.";
+// let sentence = "This dinner is not that bad! You cook well";
+// let sentence = "This movie is bad, I do not want to see it again!";
+// let sentence = "This dinner is awful!";
+
+wordNot = sentence.indexOf("not");
+wordBad = sentence.indexOf("bad");
+
+console.log(wordNot)
+console.log(wordBad)
+
+if (wordNot < wordBad && wordNot != -1 && wordBad != -1) {
+    console.log(sentence.slice(0, wordNot) + "good" + sentence.slice(wordBad+3))
+} else {
+    console.log(sentence)
+}
