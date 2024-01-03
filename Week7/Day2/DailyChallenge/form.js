bd3b040bb8b3d15021177aae330f7bd4e708f89f
slayerstokely <stokely@slayers.ru>
2024-01-03 15:50:18 +0200

    daily challenge

--- Week7/Day2/DailyChallenge/form.js
+++ Week7/Day2/DailyChallenge/form.js
@@ -2,3 +2,14 @@
 
 // 1. Create a form with two fields (name, last name) and a submit button.
 // 2. When you click the Send button, retrieve the data from the inputs, and append it on the DOM as a JSON string.
+
+const form = document.querySelector('form');
+const info = document.getElementById('info');
+
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const jsonStr = document.createElement('p');
+    jsonStr.textContent = JSON.stringify(Object.fromEntries(formData));
+    info.append(jsonStr);
+});
